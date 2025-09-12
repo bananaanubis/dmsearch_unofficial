960278982b51eeca3d9bdd1dd70e76cbaafe68ef
girlslegalgames <124251008+girlslegalgame@users.noreply.github.com>
2025-09-13 05:27:40 +0900

    Update get_card_details.php

--- get_card_details.php
+++ get_card_details.php
@@ -53,6 +53,7 @@ function format_text_for_display($raw_text, $is_ability) {
         '{SS}' => '<img src="parts/card_list_silentskill.webp" alt="Silent_Skill" class="text-icon">',
         '{WS}' => '<img src="parts/card_list_wavestriker.webp" alt="Wave_Striker" class="text-icon">',
         '{MM}' => '<img src="parts/card_list_metamorph.webp" alt="Metamorph" class="text-icon">',
+        '{AC}' => '<img src="parts/card_list_accel" alt="Accel" class="text-icon">',
     ];
 
     $lines = explode("\n", $raw_text);
